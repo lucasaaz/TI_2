a7c8fc7d9bc93345666f78955ccc929b6d84a0e0
lucasaaz <lucas.augustoaz@gmail.com>
2022-08-23 00:29:03 -0300

    Terceiro

--- Unidade01/src/SomarDoisNumeros.java
+++ Unidade01/src/SomarDoisNumeros.java
@@ -6,21 +6,32 @@ public class SomarDoisNumeros {
 		Scanner sc = new Scanner (System.in);
 		
 		//variaveis
-		int soma;
-		int x;
-		int y;
+		float soma;
+		double delta;
+		double raiz1;
+		double raiz2;
+		int a;
+		int b;
+		int c;
 		
 		//coletar dados
-		System.out.print("Digite o primeiro numero: ");
-		x = sc.nextInt();
-		System.out.print("Digite o segundo numero: ");
-		y = sc.nextInt();
+		System.out.print("Digite o primeiro numero a: ");
+		a = sc.nextInt();
+		System.out.print("Digite o segundo numero b: ");
+		b = sc.nextInt();
+		System.out.print("Digite o terceiro numero c: ");
+		c = sc.nextInt();
 		
 		//calular
-		soma= x + y;
+		soma = (b*b)- (4*a*c); 
+		delta = (double) Math.sqrt(soma);
+		raiz1 = (-b + delta)/ 2*a;
+		raiz2 = (-b - delta)/ 2*a;
 		
+		System.out.println("Soma: " + soma);
+		System.out.println("Delta: " + delta);
 		//resultado
-		System.out.println("Resultado da soma: " + soma);
+		System.out.println("Resultado da 1°raiz: " + raiz1 + "\n" + "Resultado da 2°raiz: " + raiz2);
 		
 		
 	}
